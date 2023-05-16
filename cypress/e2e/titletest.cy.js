d8fe10f12832fba03094933f21b2aa3e862a8935
QAEngineerVPB <testconsultantvinuta@gmail.com>
2023-05-16 12:55:31 +0530

    Commiting all the files

--- cypress/e2e/titletest.cy.js
+++ cypress/e2e/titletest.cy.js
@@ -10,12 +10,4 @@ describe('MyTestSuite', () => {
     cy.title().should('eq','JavaScript Component Testing and E2E Testing Framework | Cypress')
    
   })
-
-  it('NegativescenarioTestCaseId -02', () => {
-    cy.visit('https://cypress.io/')
-      cy.title().should('eq','JavaScript Component Testing and E2E Testing Framework | Cypress101')
-    //cy.get('.gb_Ve').should('be.visible').click()
-    
-  })
-
 })
